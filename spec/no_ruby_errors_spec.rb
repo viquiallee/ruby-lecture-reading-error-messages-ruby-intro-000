9388d2e8a78fcf7521f72c68e26c6303b7ff6af6
Victoria Alee <victoriaallee@gmail.com>
2020-11-10 00:04:06 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -17,7 +17,7 @@ describe "Not having any errors and being all green" do
   end
 
   context 'TypeError' do
-    it 'raises a TypeError for objects of the wrong type' do
+    it 'raises a TypeError for objects of the wrong type' 
       expect{
         load './lib/a_type_error.rb'
       }.to_not raise_error
